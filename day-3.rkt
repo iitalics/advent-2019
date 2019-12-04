9bd8a92a1e4f042f3f7100c17fd2be43f4463d52
Milo Turner <iitalics@gmail.com>
2019-12-04 13:42:29 -0500

    clean up day-3 significantly

--- day-3.rkt
+++ day-3.rkt
@@ -1,75 +1,34 @@
 #lang racket
 
-;; [vectorof real] -> real
-(define (manhattan xs)
-  (for/sum ([x (in-vector xs)])
-    (abs x)))
-
-
-;; left, right : real
+;; left, right : T
 (struct interval [left right] #:transparent)
 
-;; real real -> interval
+;; T T -> [intervalof T]
 (define (make-interval l r)
   (interval (min l r) (max l r)))
 
-;; interval real -> boolean
+;; [intervalof T] T -> boolean
 (define (interval-contains? i x)
   (<= (interval-left i) x (interval-right i)))
 
-;; [hash real => [listof interval]]
-;; [hash real => [listof interval]]
-;;   -> [listof [vector real real]]
-(define (interval-intersections x=>y-intervals
-                                y=>x-intervals)
-  (for*/list ([(x yis) (in-hash x=>y-intervals)]
-              [yi (in-list yis)]
-              [(y xis) (in-hash y=>x-intervals)]
-              #:when (interval-contains? yi y)
-              [xi (in-list xis)]
-              #:when (interval-contains? xi x))
-    (vector x y)))
-
-
-;; axis : (or 'x 'y)
-;; sign : (or -1 +1)
+;; ux, uy : (or -1 +1)
 ;; len : integer
-(struct path [axis sign len] #:transparent)
-(define (L len) (path 'x -1 len))
-(define (R len) (path 'x +1 len))
-(define (U len) (path 'y -1 len))
-(define (D len) (path 'y +1 len))
+(struct path [ux uy len] #:transparent)
+(define (L len) (path -1  0 len))
+(define (R len) (path +1  0 len))
+(define (U len) (path  0 -1 len))
+(define (D len) (path  0 +1 len))
+
+;; path -> (or 'h 'v)
+(define (path-axis p)
+  (if (zero? (path-ux p)) 'v 'h))
 
 ;; path integer integer -> integer integer
 (define (path-endpoint p x y)
-  (define δ (* (path-len p) (path-sign p)))
-  (match (path-axis p)
-    ['x (values (+ x δ) y)]
-    ['y (values x (+ y δ))]))
+  (values (+ x (* (path-ux p) (path-len p)))
+          (+ y (* (path-uy p) (path-len p)))))
 
-;; ... [hash real => [listof interval]] ...
-(struct grid [x=>y-intervals y=>x-intervals] #:transparent)
-
-(define (paths->grid paths)
-  (define (hash-push h k x)
-    (hash-update h k (λ (xs) (cons x xs)) '()))
-  (for/fold ([x=>yi (hash)] [y=>xi (hash)]
-             [x 0] [y 0]
-             #:result (grid x=>yi y=>xi))
-            ([p (in-list paths)])
-    (define-values [x* y*] (path-endpoint p x y))
-    (define-values [x=>yi* y=>xi*]
-      (match (path-axis p)
-        ['x (values x=>yi (hash-push y=>xi y (make-interval x x*)))]
-        ['y (values (hash-push x=>yi x (make-interval y y*)) y=>xi)]))
-    (values x=>yi* y=>xi* x* y*)))
-
-(define (grid-intersections g1 g2)
-  (append (interval-intersections (grid-x=>y-intervals g1)
-                                  (grid-y=>x-intervals g2))
-          (interval-intersections (grid-x=>y-intervals g2)
-                                  (grid-y=>x-intervals g1))))
-
+;; -> path
 (define (read-path [port (current-input-port)])
   (match (regexp-match #px",?([RULD])(\\d+)" port)
     [(list _ dir len)
@@ -77,14 +36,49 @@
       (string->number (bytes->string/utf-8 len)))]
     [_ eof]))
 
-(define (read-wires [port (current-input-port)])
-  (for/list ([l (in-lines port)])
-    (with-input-from-string l
-      (λ () (for/list ([p (in-port read-path)]) p)))))
+;; pos : integer
+;; int : [intervalof integer]
+(struct seg [pos int] #:transparent)
 
+;; [listof seg] [listof seg] -> [listof (cons seg seg)]
+(define (seg-intersections segs-1 segs-2)
+  (for*/list ([s1 (in-list segs-1)]
+              [s2 (in-list segs-2)]
+              #:when (and (interval-contains? (seg-int s1) (seg-pos s2))
+                          (interval-contains? (seg-int s2) (seg-pos s1))))
+    (cons s1 s2)))
+
+;; h-segs, v-segs : [listof seg]
+(struct grid [h-segs v-segs] #:transparent)
+
+;; grid grid -> [listof (cons seg seg)]
+(define (grid-intersections g1 g2)
+  (append (seg-intersections (grid-v-segs g1) (grid-h-segs g2))
+          (seg-intersections (grid-v-segs g2) (grid-h-segs g1))))
+
+;; [listof path] -> grid
+(define (paths->grid paths)
+  (for/fold ([vs '()] [hs '()]
+             [x 0] [y 0]
+             #:result (grid vs hs))
+            ([p (in-list paths)])
+    (define-values [x* y*] (path-endpoint p x y))
+    (define-values [vs* hs*]
+      (match (path-axis p)
+        ['h (values vs (cons (seg y (make-interval x x*)) hs))]
+        ['v (values (cons (seg x (make-interval y y*)) vs) hs)]))
+    (values vs* hs* x* y*)))
+
+;; g1, g2 : grid
 (match-define (list (app paths->grid g1) (app paths->grid g2))
-  (with-input-from-file "input/day-3.txt" read-wires))
+  (with-input-from-file "input/day-3.txt"
+    (λ ()
+      (for/list ([l (in-lines)])
+        (port->list read-path (open-input-string l))))))
 
-(apply min (for/list ([v (in-list (grid-intersections g1 g2))]
-                      #:when (not (equal? v #(0 0))))
-             (manhattan v)))
+(define intersects
+  (grid-intersections g1 g2))
+
+(apply min (for/list ([ss (in-list intersects)])
+             (match-define (cons (seg x _) (seg y _)) ss)
+             (+ (abs x) (abs y))))

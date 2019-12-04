99845e97ce0ec323589cf2a855a99bc0279345d5
Milo Turner <iitalics@gmail.com>
2019-12-04 15:14:26 -0500

    extra cheeky day 4

--- day-4.rkt
+++ day-4.rkt
@@ -1,19 +1,18 @@
 #lang racket
 
 ;; nat -> [listof (between 0 9)]
-(define (digits x)
-  (let loop ([x x] [a '()])
-    (if (<= x 10)
-        (cons x a)
-        (let-values ([(x* d) (quotient/remainder x 10)])
-          (loop x* (cons d a))))))
+(define (digits x [a '()])
+  (if (<= x 10)
+      (cons x a)
+      (let-values ([(x* d) (quotient/remainder x 10)])
+        (digits x* (cons d a)))))
 
-;; [nat -> boolean] -> nat -> boolean
-(define ((meets-criteria? len?) n)
+;; [nat nat -> boolean] -> nat -> boolean
+(define ((meets-criteria? cmp) n)
   (define digs (digits n))
   (and (apply <= digs)
        (for/or ([g (in-list (group-by values digs))])
-         (len? (length g)))))
+         (cmp (length g) 2))))
 
 ; [listof integer]
 (define passwords
@@ -22,5 +21,5 @@
      (range (string->number (bytes->string/utf-8 lo))
             (string->number (bytes->string/utf-8 hi)))]))
 
-(count (meets-criteria? (>=/c 2)) passwords)
-(count (meets-criteria? (=/c 2)) passwords)
+(count (meets-criteria? >=) passwords)
+(count (meets-criteria? =) passwords)
